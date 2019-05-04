47c1c7347a7b7c4f339cae6fe3ca23bab9823c79
saiteja3 <42633885+saiteja3@users.noreply.github.com>
2019-05-04 16:05:48 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 class helloworld{
 void getdata(){
 System.out.println("hello world");}
-public static void mian(string args[]){
+public static void mian(String args[]){
 helloworld h=new helloworld();
 h.getdata();
 }
